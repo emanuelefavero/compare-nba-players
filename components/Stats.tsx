a1e439ec51967719646376f653792cd47a6739ef
emanuelefavero <emafaux89@gmail.com>
2023-03-02 22:34:12 +0100

    add all stats cards

--- components/Stats.tsx
+++ components/Stats.tsx
@@ -56,9 +56,117 @@ export default function Stats({
       <StatsCard
         statisticName='3P%'
         statisticNameFull='3-Point Percentage'
-        // remove first digit (0) and round to 2 decimal places
         player1Statistic={player1Stats.fg3_pct.toFixed(2).slice(1)}
         player2Statistic={player2Stats.fg3_pct.toFixed(2).slice(1)}
+        player1Name={`${player1.first_name} ${player1.last_name}`}
+        player2Name={`${player2.first_name} ${player2.last_name}`}
+      />
+
+      <StatsCard
+        statisticName='FT%'
+        statisticNameFull='Free Throw Percentage'
+        player1Statistic={player1Stats.ft_pct.toFixed(2).slice(1)}
+        player2Statistic={player2Stats.ft_pct.toFixed(2).slice(1)}
+        player1Name={`${player1.first_name} ${player1.last_name}`}
+        player2Name={`${player2.first_name} ${player2.last_name}`}
+      />
+
+      <StatsCard
+        // force a space before FG
+        statisticName={'\u00A0FG'}
+        statisticNameFull='Field Goals Made'
+        player1Statistic={player1Stats.fgm.toFixed(1)}
+        player2Statistic={player2Stats.fgm.toFixed(1)}
+        player1Name={`${player1.first_name} ${player1.last_name}`}
+        player2Name={`${player2.first_name} ${player2.last_name}`}
+      />
+
+      <StatsCard
+        statisticName={'FGA'}
+        statisticNameFull='Field Goals Attempted'
+        player1Statistic={player1Stats.fga.toFixed(1)}
+        player2Statistic={player2Stats.fga.toFixed(1)}
+        player1Name={`${player1.first_name} ${player1.last_name}`}
+        player2Name={`${player2.first_name} ${player2.last_name}`}
+      />
+
+      <StatsCard
+        statisticName={'\u00A03P'}
+        statisticNameFull='3-Point Field Goals Made'
+        player1Statistic={player1Stats.fg3m.toFixed(1)}
+        player2Statistic={player2Stats.fg3m.toFixed(1)}
+        player1Name={`${player1.first_name} ${player1.last_name}`}
+        player2Name={`${player2.first_name} ${player2.last_name}`}
+      />
+
+      <StatsCard
+        statisticName={'\u00A0FT'}
+        statisticNameFull='Free Throws Made'
+        player1Statistic={player1Stats.ftm.toFixed(1)}
+        player2Statistic={player2Stats.ftm.toFixed(1)}
+        player1Name={`${player1.first_name} ${player1.last_name}`}
+        player2Name={`${player2.first_name} ${player2.last_name}`}
+      />
+
+      <StatsCard
+        statisticName={'FTA'}
+        statisticNameFull='Free Throws Attempted'
+        player1Statistic={player1Stats.fta.toFixed(1)}
+        player2Statistic={player2Stats.fta.toFixed(1)}
+        player1Name={`${player1.first_name} ${player1.last_name}`}
+        player2Name={`${player2.first_name} ${player2.last_name}`}
+      />
+
+      <StatsCard
+        statisticName={'ORB'}
+        statisticNameFull='Offensive Rebounds'
+        player1Statistic={player1Stats.oreb.toFixed(1)}
+        player2Statistic={player2Stats.oreb.toFixed(1)}
+        player1Name={`${player1.first_name} ${player1.last_name}`}
+        player2Name={`${player2.first_name} ${player2.last_name}`}
+      />
+
+      <StatsCard
+        statisticName={'DRB'}
+        statisticNameFull='Defensive Rebounds'
+        player1Statistic={player1Stats.dreb.toFixed(1)}
+        player2Statistic={player2Stats.dreb.toFixed(1)}
+        player1Name={`${player1.first_name} ${player1.last_name}`}
+        player2Name={`${player2.first_name} ${player2.last_name}`}
+      />
+
+      <StatsCard
+        statisticName={'STL'}
+        statisticNameFull='Steals'
+        player1Statistic={player1Stats.stl.toFixed(1)}
+        player2Statistic={player2Stats.stl.toFixed(1)}
+        player1Name={`${player1.first_name} ${player1.last_name}`}
+        player2Name={`${player2.first_name} ${player2.last_name}`}
+      />
+
+      <StatsCard
+        statisticName={'BLK'}
+        statisticNameFull='Blocks'
+        player1Statistic={player1Stats.blk.toFixed(1)}
+        player2Statistic={player2Stats.blk.toFixed(1)}
+        player1Name={`${player1.first_name} ${player1.last_name}`}
+        player2Name={`${player2.first_name} ${player2.last_name}`}
+      />
+
+      <StatsCard
+        statisticName={'TOV'}
+        statisticNameFull='Turnovers'
+        player1Statistic={player1Stats.turnover.toFixed(1)}
+        player2Statistic={player2Stats.turnover.toFixed(1)}
+        player1Name={`${player1.first_name} ${player1.last_name}`}
+        player2Name={`${player2.first_name} ${player2.last_name}`}
+      />
+
+      <StatsCard
+        statisticName={'\u00A0PF'}
+        statisticNameFull='Personal Fouls'
+        player1Statistic={player1Stats.pf.toFixed(1)}
+        player2Statistic={player2Stats.pf.toFixed(1)}
         player1Name={`${player1.first_name} ${player1.last_name}`}
         player2Name={`${player2.first_name} ${player2.last_name}`}
       />
