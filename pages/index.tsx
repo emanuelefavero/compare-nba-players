c5ff4aef663e6f01f14a20e2a22e12ac47f93272
emanuelefavero <emafaux89@gmail.com>
2023-03-04 13:14:57 +0100

    add player info in foundPlayers.map() section, index,tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import { useState, FormEvent } from 'react'
+import Image from 'next/image'
 import Stats from '@/components/Stats'
 import Header from '@/components/Header'
 
@@ -32,9 +33,36 @@ export default function Home() {
       <main>
         {foundPlayers.map((player: IPlayer) => (
           <div key={player.id}>
-            <p>
+            {/* Player Info ----- */}
+            <h2>
               {player.first_name} {player.last_name}
-            </p>
+            </h2>
+
+            {player.team.abbreviation && (
+              <Image
+                src={`/nba-logos/${player.team.abbreviation}.svg`}
+                alt='logo'
+                width={40}
+                height={40}
+              />
+            )}
+
+            <h3>
+              {player.team.city} {player.team.name}
+            </h3>
+
+            <h4>{player.team.conference}</h4>
+
+            <p>{player.position}</p>
+
+            {player.height_feet && player.height_inches && (
+              <h4>{`${player.height_feet}' ${player.height_inches}''`}</h4>
+            )}
+
+            {player.weight_pounds && <h4>{`${player.weight_pounds} lbs`}</h4>}
+
+            {/* ----- */}
+
             <button
               onClick={() => {
                 setPlayer1(player)
