288bf9e869bc437bd22abbc4f74d9cfe1806997d
emanuelefavero <emafaux89@gmail.com>
2023-03-08 19:59:31 +0100

    add more contrast to team colors

--- utils/getTeamColor.ts
+++ utils/getTeamColor.ts
@@ -6,64 +6,64 @@ export default function getTeamColor(
     case 'Hawks':
       return `rgba(224,58,62,${opacity})`
     case 'Celtics':
-      return `rgba(0,122,51,${opacity})`
+      return `rgba(2,131,70,${opacity})`
     case 'Nets':
       return `rgba(60,60,60,${opacity})`
     case 'Hornets':
-      return `rgba(29,17,96,${opacity})`
+      return `rgba(2,121,141,${opacity})`
     case 'Bulls':
-      return `rgba(206,17,65,${opacity})`
+      return `rgba(206,6,62,${opacity})`
     case 'Cavaliers':
-      return `rgba(134,0,56,${opacity})`
+      return `rgba(108,35,58,${opacity})`
     case 'Mavericks':
-      return `rgba(0,83,188,${opacity})`
+      return `rgba(0,126,199,${opacity})`
     case 'Nuggets':
       return `rgba(20,51,96,${opacity})`
     case 'Pistons':
-      return `rgba(200,16,46,${opacity})`
+      return `rgba(202,6,41,${opacity})`
     case 'Warriors':
-      return `rgba(29,66,138,${opacity})`
+      return `rgba(27,63,130,${opacity})`
     case 'Rockets':
-      return `rgba(206,17,65,${opacity})`
+      return `rgba(195,18,61,${opacity})`
     case 'Pacers':
-      return `rgba(0,45,98,${opacity})`
+      return `rgba(241,177,46,${opacity})`
     case 'Clippers':
-      return `rgba(200,16,46,${opacity})`
+      return `rgba(190,16,44,${opacity})`
     case 'Lakers':
-      return `rgba(85,37,130,${opacity})`
+      return `rgba(81,36,125,${opacity})`
     case 'Grizzlies':
-      return `rgba(93,118,169,${opacity})`
+      return `rgba(88,112,161,${opacity})`
     case 'Heat':
-      return `rgba(152,0,46,${opacity})`
+      return `rgba(144,1,45,${opacity})`
     case 'Bucks':
-      return `rgba(0,71,27,${opacity})`
+      return `rgba(12,67,30,${opacity})`
     case 'Timberwolves':
-      return `rgba(12,35,64,${opacity})`
+      return `rgba(12,32,60,${opacity})`
     case 'Pelicans':
-      return `rgba(0,22,65,${opacity})`
+      return `rgba(0,44,130,${opacity})`
     case 'Knicks':
-      return `rgba(0,107,182,${opacity})`
+      return `rgba(232,126,36,${opacity})`
     case 'Thunder':
-      return `rgba(0,125,195,${opacity})`
+      return `rgba(0,122,193,${opacity})`
     case 'Magic':
-      return `rgba(0,125,195,${opacity})`
+      return `rgba(11,113,180,${opacity})`
     case '76ers':
-      return `rgba(237,23,76,${opacity})`
+      return `rgba(27,63,130,${opacity})`
     case 'Suns':
-      return `rgba(229,95,32,${opacity})`
+      return `rgba(217,91,31,${opacity})`
     case 'Trail Blazers':
-      return `rgba(224,58,62,${opacity})`
+      return `rgba(208,4,39,${opacity})`
     case 'Kings':
-      return `rgba(91,43,130,${opacity})`
+      return `rgba(87,42,123,${opacity})`
     case 'Spurs':
-      return `rgba(196,206,211,${opacity})`
+      return `rgba(186,195,201,${opacity})`
     case 'Raptors':
-      return `rgba(206,17,65,${opacity})`
+      return `rgba(180,26,31,${opacity})`
     case 'Jazz':
-      return `rgba(0,43,92,${opacity})`
+      return `rgba(18,48,91,${opacity})`
     case 'Wizards':
-      return `rgba(0,43,92,${opacity})`
+      return `rgba(215,23,52,${opacity})`
     default:
-      return `rgba(0,0,0,${opacity})`
+      return `rgba(45,55,75,${opacity})`
   }
 }
